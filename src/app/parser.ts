546df23e273382befb2f7e2b32d691624be2782c
Joel Tanzi <jtanziredux@gmail.com>
2018-04-21 17:30:52 -0500

    Corrected some issues with the parser.

--- src/app/parser.ts
+++ src/app/parser.ts
@@ -1,58 +1,66 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
-declare var System: any;   
-// var json = require('./dictionary.json');
-
-
-
+declare var System: any;
+// let json = require('./dictionary.json');
+import * as data from './dictionary.json';
 
 export class Parser {
-
-
 
     results: any;
     dictionary: any;
 
     constructor( ) {
-        System.import('./dictionary.json').then(file => {
-            this.dictionary = file;
-         });
-        
+        // System.import('./dictionary.json').then(file => {
+        //     this.dictionary = file;
+        //  });
+        this.dictionary = data;
     }
 
 
-    getDictionary(): void {
+    private getDictionary(): void {
         console.dir(this.dictionary);
     }
 
     /*
         name: getWords
-        params: 
+        params:
             samples: The array of strings to test against.
         Returns: An array containing the dictionary values matching the words that have been matched.
     */
     getWords(samples: string[]): string[] {
+        console.dir(samples);
         let regexString = '';
         Object.keys(this.dictionary).forEach((key) => {
-            console.log(key);
+            // console.log(key);
             regexString += key + '|';
         });
         const regex = new RegExp(regexString.substring(0, regexString.length - 1));
-        console.log(regex);
-        console.log(samples);
-        let words = [];
+        // console.log(regex);
+        // console.log(samples);
+        const words = [];
         samples.forEach((s) => {
             if (regex.test(s)) {
-                console.log(regex.exec(s)[0]);
                 words.push(s.match(regex)[0]);
+            } else {
+                words.push(s);
             }
+        });
 
-        });
+        // console.dir(words);
         const newWords = words.map((word) => {
-            return this.dictionary[word];
+            if (this.dictionary[word]) {
+                return this.dictionary[word];
+            } else {
+                return word;
+            }
         })
-        const uniqueWords = newWords.filter((elem, index, self) => {
-            return index === self.indexOf(elem);
-        });
+        console.dir(newWords);
+        const uniqueWords = this.uniqueWords(newWords);
         return uniqueWords;
+    }
+
+    uniqueWords(arr: string[]) {
+        return arr.sort().filter((item, pos, ar) => {
+            return !pos || item != ar[pos - 1];
+        })
     }
 }
